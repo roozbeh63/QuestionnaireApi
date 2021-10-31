7d9e3d9b22f1e43ac19bfaa6f6b1f438ee3d2056
Roozbeh <rhalvaei@gmail.com>
2021-10-31 09:48:47 +0100

    get all questions
    get all questions of an questionnaire based on the application name of the questionnaire

--- QuestionnaireApi/Services/QuestionnaireService.cs
+++ QuestionnaireApi/Services/QuestionnaireService.cs
@@ -1,13 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using QuestionnaireApi.Helpers;
+using QuestionnaireApi.Models;
+using System.Data;
 using System.Threading.Tasks;
 
 namespace QuestionnaireApi.Services
 {
-    public class QuestionnaireService : BaseService<QuestionnaireApi.Models.Questionnaire>
+    public class QuestionnaireService : BaseService<Questionnaire>
     {
         public QuestionnaireService(QAContext context)
         : base(context) { }
+
+        public async Task<DataTable> GetQuestionOfQuestionnaireAsync(string appName)
+        {
+            return await this.context.RawSqlQueryAsync($"select * from {new Questionnaire().TableName}", new System.Collections.Generic.Dictionary<string, object>
+            {
+                { "app", appName }
+            });
+        }
     }
 }
